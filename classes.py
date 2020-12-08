157643339f8f0629f2a972952a1ba1fadfcd24c0
Thibault Doucet <thibault.doucet44@gmail.com>
2020-12-08 09:07:17 +0100

    :sparkles: Init project

---
+++ classes.py
@@ -0,0 +1,41 @@
+import json
+import requests
+from exceptions import *
+
+class Meteo:
+    token = "d54887352b0771a0185439e4d71da42d07392d1b1e0a8971b37485ea54a437a0"
+    code_insee = "2011101"
+    api_url = 'https://api.meteo-concept.com/api/forecast/daily/0?token=' + token + '&insee=' + code_insee
+    headers = {'Content-Type': 'application/json'}
+    weather = 0
+    tmin = 0
+    tmax = 0
+
+    def __init__(self):
+        print("Code insee météo : " + self.code_insee)
+        print("Token API : " + self.token)
+        print("API URL : " + self.api_url)
+        self.new_query()
+
+    def new_query(self):
+        """
+            Permet de faire appel à l'API météo l'URL a été définie dans la construction de la classe.
+            Le résultat de la requête est stocké dans les attributs de la classe
+        """
+        response = requests.get(self.api_url, headers=self.headers)
+        if response.status_code == 200:
+            result = json.loads(response.content.decode('utf-8'))
+            self.weather = result['forecast']['weather']
+            self.tmin = result['forecast']['tmin']
+            self.tmax = result['forecast']['tmax']
+        else:
+            raise ApiError('Impossible de récupérer la méteo depuis l\'API')
+
+
+    def get_meteo(self):
+        if self.weather == 0:
+            return "Beau temps"
+        return "Mauvais temps"
+
+    def get_tmp(self):
+        return [self.tmin, self.tmax]
---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5 import QtCore
+from classes import Meteo
+
+meteo = Meteo()
+
+def update_labels():
+    labelMeteo.setText("Météo sur Marseille : %s (Min: %d°C / Max: %d°C)" % (meteo.get_meteo(), meteo.get_tmp()[0], meteo.get_tmp()[1]))
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+
+    w.resize(500, 120)
+    w.move(300, 300)
+    w.setWindowTitle('Car Manager')
+
+    # Layout
+    layout = QVBoxLayout()
+    layoutBoutons = QHBoxLayout()
+
+    labelMeteo = QLabel("")
+
+    label = QLabel("")
+    label.setWordWrap(True)
+    previous = QPushButton("Supprimer un car")
+    next = QPushButton("Ajouter un car")
+
+    layoutBoutons.addWidget(previous)
+    layoutBoutons.addWidget(next)
+
+    layout.addWidget(labelMeteo)
+    layout.addWidget(label)
+    layout.addLayout(layoutBoutons)
+
+    # Update label every 10ms
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update_labels)
+    timer.start(10)
+
+    w.setLayout(layout)
+    w.show()
+
+    app.exec_()
+
+    #sys.exit(app.exec_())
